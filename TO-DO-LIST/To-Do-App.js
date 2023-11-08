1427c9c28a1f48945c6f5271bd2b1a5a0c0d87c5
ksurya2002 <kuppansurya2002@gmail.com>
2023-11-08 22:31:13 +0530

    add  date and edit button

--- TO-DO-LIST/To-Do-App.js
+++ TO-DO-LIST/To-Do-App.js
@@ -1,39 +1,45 @@
+
+
 let myform = document.getElementById("myform");
 let myinput = document.getElementById("myinput");
 let mysubmit = document.getElementById("mysubmit");
+let mydate= document.getElementById("mydate");
+
 
 
 myform.addEventListener("submit",
     (data) => {
         data.preventDefault();
-        createItem(myinput.value);
-
+        createItem(myinput.value,mydate.value)
 
     });
 
 function deleteItem(d) {
     d.parentElement.remove()
 }
+function editItem(e) {
+    const li = e.parentElement;
+    const edit = (myinput.value);
+    li.firstChild.textContent=edit;
+}
+createItem = (v,date)  => {
 
-createItem = (v) => {
-
-    let template = `<li>${v}<button onclick="deleteItem(this)">Delete</button></li>`;
+    let template = `<ul><li>${v}-${date}<br><button class="edit" onclick="editItem(this)">Edit</button>
+    <button class="delete" onclick="deleteItem(this)">Delete</button></li></ul>`;
     mylist.insertAdjacentHTML("beforeend", template);
     myinput.value = "";
+    mydate.value = "";
+    //  myinput.focus();
+   // mysubmit.disabled = true;
 
-    myinput.focus();
-    mysubmit.disabled = true;
-  
 
 }
+// myinput.addEventListener("input", function button() {
+//     if (myinput.value.trim() === "") {
+//         mysubmit.disabled = true;
+//     }
+//     else if (myinput.value.trim() !== "") {
+//         mysubmit.disabled = false;
+//     }
 
-
-myinput.addEventListener("input", function button() {
-    if (myinput.value.trim() === "") {
-        mysubmit.disabled = true;
-    }
-    else if (myinput.value.trim() !== "") {
-        mysubmit.disabled = false;
-    }
-
-});
+// });
